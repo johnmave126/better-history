d48f4a6d067421d869bc3a68bd19644baef4bef7
Roy Kolak <roy.kolak@gmail.com>
2011-08-04 15:18:26 -0500

    fixed odd 'remove' property

--- extension/scripts/history_view.js
+++ extension/scripts/history_view.js
@@ -37,38 +37,40 @@ function HistoryView() {
       for(date in results) {
         var dateSection = $('#dateTemplate').tmpl({date:date});
         for(time in results[date]) {
-          var timeSection = $('#timeTemplate').tmpl({time:time}).appendTo(dateSection);
-          $.each(results[date][time], function(i, historyItem) {
-            if(isArray(historyItem)) {
-              var historyItems = historyItem;
-              $('#combineVisitsTemplate').tmpl({
-                url: historyItems[0].url,
-                amount: historyItems.length,
-                domain: historyItems[0].domain()
-              }).appendTo(timeSection);
+          if(results[date].hasOwnProperty(time)) {
+            var timeSection = $('#timeTemplate').tmpl({time:time}).appendTo(dateSection);
+            $.each(results[date][time], function(i, historyItem) {
+              if(isArray(historyItem)) {
+                var historyItems = historyItem;
+                $('#combineVisitsTemplate').tmpl({
+                  url: historyItems[0].url,
+                  amount: historyItems.length,
+                  domain: historyItems[0].domain()
+                }).appendTo(timeSection);
 
-              var expandedVisits = $('#expandedVisitsTemplate').tmpl({});
-              $.each(historyItems, function(i, historyItem) {
-                $('#pageVisitTemplate').tmpl(historyItem).appendTo(expandedVisits);
-              });
-              $(timeSection).append(expandedVisits);
-            } else {
-              $('#pageVisitTemplate').tmpl(historyItem).appendTo(timeSection);
-            }
-          });
-          $(container).append(dateSection);
-          $('.expand').unbind('click').click(function(ev) {
-            ev.preventDefault();
-            var element = ev.target;
-            if($(element).hasClass('active')) {
-              $(element).text('Expand');
-              $(element).parents('.page').next().slideUp('fast');
-            } else {
-              $(element).text('Collapse');
-              $(element).parents('.page').next().slideDown('fast');
-            }
-            $(element).toggleClass('active');
-          });
+                var expandedVisits = $('#expandedVisitsTemplate').tmpl({});
+                $.each(historyItems, function(i, historyItem) {
+                  $('#pageVisitTemplate').tmpl(historyItem).appendTo(expandedVisits);
+                });
+                $(timeSection).append(expandedVisits);
+              } else {
+                $('#pageVisitTemplate').tmpl(historyItem).appendTo(timeSection);
+              }
+            });
+            $(container).append(dateSection);
+            $('.expand').unbind('click').click(function(ev) {
+              ev.preventDefault();
+              var element = ev.target;
+              if($(element).hasClass('active')) {
+                $(element).text('Expand');
+                $(element).parents('.page').next().slideUp('fast');
+              } else {
+                $(element).text('Collapse');
+                $(element).parents('.page').next().slideDown('fast');
+              }
+              $(element).toggleClass('active');
+            });
+          }
         }
       }
       stickHeaders(container);
