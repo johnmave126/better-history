66056de72a2e0f460457364b52baa476b2c7bc16
Roy Kolak <roy.kolak@gmail.com>
2011-08-11 17:43:01 -0500

    reorganizing specs and scripts

--- extension/javascripts/views/filter_view.js
+++ extension/javascripts/views/filter_view.js
@@ -6,12 +6,13 @@ FilterView = Backbone.View.extend({
   render: function(results) {
     var self = this;
     $('#filterViewTemplate').tmpl(this.model.toJSON()).appendTo($(this.el));
+    $('.content', this.el).hide();
     $(this.el).fadeIn("fast", function() {
       Visit.search(self.model.options(), function(results) {
         dateVisits = groupResults(results);
         $.each(dateVisits.models, function(i, dateVisit) {
           var dateVisitView = new DateVisitView({model: dateVisit});
-          $('.content', self.el).append(dateVisitView.render().el);
+          $('.content', self.el).append(dateVisitView.render().el).fadeIn("fast");
         });
         self.stickHeaders($('.content', self.el));
       });
