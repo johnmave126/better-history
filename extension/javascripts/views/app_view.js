5a36c51668348027152fdd8030383b732cd96927
Roy Kolak <roy.kolak@gmail.com>
2011-08-12 15:14:58 -0500

    smoother filters animation

--- extension/javascripts/views/app_view.js
+++ extension/javascripts/views/app_view.js
@@ -2,7 +2,7 @@ AppView = Backbone.View.extend({
   render: function() {
     var filtersView = new FiltersView({collection: filters});
 
-    $('.navbar', this.el).append(filtersView.render().el).hide().fadeIn("fast");
+    $('.navbar', this.el).append(filtersView.render().el).hide().fadeIn(200);
 
     return this;
   }
