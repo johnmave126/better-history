10d9263e1a6c10413d9e37abf6919789b9a5e698
Roy Kolak <roy.kolak@gmail.com>
2011-08-19 15:26:20 -0500

    Improved coverage and refactored delete view

--- extension/javascripts/views/delete_view.js
+++ extension/javascripts/views/delete_view.js
@@ -1,41 +1,56 @@
 DeleteView = Backbone.View.extend({
   tagName: 'div',
-  className: 'delete_zone',
+  className: 'delete_view',
+
+  overClass: 'over',
 
   render: function() {
-    $('#deleteZoneTemplate').tmpl().appendTo(this.el);
-
     var self = this;
+    $('#deleteZoneTemplate').tmpl().appendTo(this.el);
     $(this.el).droppable({
       tolerance: 'pointer',
-      over: function() {
-        $(self.el).addClass('over');
-      },
-      out: function() {
-        self.deactivateArea();
-      },
-      drop: function(ev, ui) {
-        if($(ui.draggable).hasClass('grouped_visit')) {
-          $(ui.draggable).next().find('.page_visit').each(function() {
-            self.getPageVisit(this).destroy();
-          });
-          $(ui.draggable).next().slideUp();
-        } else {
-          self.getPageVisit(ui.draggable).destroy();
-        }
-
-        $(ui.draggable).slideUp("fast");
-        self.deactivateArea();
-      }
+      over: function() { self.over(); },
+      out: function() { self.out(); },
+      drop: function(ev, ui) { self.drop(ui.draggable); }
     });
     return this;
   },
 
-  getPageVisit: function(element) {
+  over: function() {
+    $(this.el).addClass(this.overClass);
+  },
+
+  out: function() {
+    $(this.el).removeClass(this.overClass);
+  },
+
+  drop: function(element) {
+    var method = (this.isGroupedVisit(element) ? 'removeGroupedVisit' : 'removePageVisit');
+    this[method](element);
+    $(element).slideUp("fast");
+    $(this.el).removeClass(this.overClass);
+  },
+
+  isGroupedVisit: function(element) {
+    return element.hasClass('grouped_visit');
+  },
+
+  getPageVisitByElement: function(element) {
     return pageVisits.getByCid($(element).attr('data-cid'));
   },
 
-  deactivateArea: function() {
-    $(this.el).removeClass('over');
+  removeGroupedVisit: function(element) {
+    var self = this;
+    $(element).next().find('.page_visit').each(function() {
+      self.removePageVisit(this);
+    });
+    $(element).next().slideUp();
+  },
+
+  removePageVisit: function(element) {
+    pageVisit = this.getPageVisitByElement(element);
+    if(pageVisit) {
+      pageVisit.destroy();
+    }
   }
 });
--- spec/javascripts/views/delete_view_spec.js
+++ spec/javascripts/views/delete_view_spec.js
@@ -1,9 +1,130 @@
 describe('DeleteView', function() {
-  it('exists', function() {
-    expect(DeleteView).toBeDefined();
+  var deleteView;
+
+  beforeEach(function() {
+    loadFixtures('delete_view.html');
+    deleteView = new DeleteView();
+  });
+
+  describe('#initialize', function() {
+    it('defines a tag name', function() {
+      expect(deleteView.tagName).toEqual('div');
+    });
+
+    it('defines a class name', function() {
+      expect(deleteView.className).toEqual('delete_view');
+    });
   });
 
   describe('#render', function() {
-    it('inserts the
+    it('clones the template into the container', function() {
+      deleteView.render();
+      expect($(deleteView.el).html()).not.toBeEmpty();
+    });
+
+    it('attaches droppable behavior', function() {
+      spyOn($.fn, 'droppable');
+      deleteView.render();
+      expect($.fn.droppable).toHaveBeenCalledWith({
+        tolerance: 'pointer',
+        over: jasmine.any(Function),
+        out: jasmine.any(Function),
+        drop: jasmine.any(Function)
+      });
+    });
+  });
+
+  describe('#over', function() {
+    it('adds the over class to the container', function() {
+      deleteView.over();
+      expect($(deleteView.el)).toHaveClass(deleteView.overClass);
+    });
+  });
+
+  describe('#out', function() {
+    it('removes the over class from the container', function() {
+      $(deleteView.el).addClass(deleteView.overClass);
+      deleteView.out();
+      expect($(deleteView.el)).not.toHaveClass(this.overClass);
+    });
+  });
+
+  describe('#drop', function() {
+    beforeEach(function() {
+      pageVisits = new PageVisits();
+    });
+
+    it('removes the over class from the container', function() {
+      $(deleteView.el).addClass(deleteView.overClass);
+      deleteView.drop($('<div></div>'));
+      expect($(deleteView.el)).not.toHaveClass(this.overClass);
+    });
+
+    it('slides up the droppable element', function() {
+      spyOn($.fn, 'slideUp');
+      deleteView.drop($('<div></div>'));
+      expect($.fn.slideUp).toHaveBeenCalledWith("fast");
+    });
+
+    it('removes a pageVisit when that was what was dropped', function() {
+      spyOn(deleteView, 'removePageVisit');
+      var element = $('<div class="page_visit"></div>');
+      deleteView.drop(element);
+      expect(deleteView.removePageVisit).toHaveBeenCalledWith(element);
+    });
+  });
+
+  describe('#removeGroupedVisit', function() {
+    it('removes all the page visits', function() {
+      spyOn(deleteView, 'removePageVisit');
+      deleteView.removeGroupedVisit($('<div></div><div><div class="page_visit"></div><div class="page_visit"></div><div class="page_visit"></div></div>'));
+      expect(deleteView.removePageVisit.argsForCall.length).toEqual(3);
+    });
+
+    it('slides up grouped visits', function() {
+      spyOn($.fn, 'slideUp');
+      deleteView.removeGroupedVisit($('<div></div><div></div>'));
+      expect($.fn.slideUp);
+    });
+  });
+
+  describe("#removePageVisit", function() {
+    var pageVisit;
+
+    beforeEach(function() {
+      pageVisit = {destroy: jasmine.createSpy('destroy')};
+      spyOn(deleteView, 'getPageVisitByElement').andReturn(pageVisit);
+    });
+
+    it('it destroys the pageVisit', function() {
+      deleteView.removePageVisit($('<div data-cid="c31"></div>'));
+      expect(pageVisit.destroy).toHaveBeenCalled();
+    });
+  });
+
+  describe('#getPageVisitByElement', function() {
+    beforeEach(function() {
+      pageVisits = new PageVisits([
+        new PageVisit({url: 'yahoo.com'}),
+        new PageVisit({url: 'google.com'})
+      ]);
+    });
+
+    it('returns the pageVisit for the passed element\'s data-cid attribute', function() {
+      var pageVisit = deleteView.getPageVisitByElement($('<div data-cid="c34"></div>'));
+      expect(pageVisit).toEqual(pageVisits.at(1));
+    });
+  });
+
+  describe('#isGroupedVisit', function() {
+    it('returns true when the dropped element has the grouped visit class', function() {
+      var groupedVisit = deleteView.isGroupedVisit($('<div class="grouped_visit"></div>'));
+      expect(groupedVisit).toBeTruthy();
+    });
+
+    it('returns false when the dropped element does not have the grouped visit class', function() {
+      var groupedVisit = deleteView.isGroupedVisit($('<div></div>'));
+      expect(groupedVisit).toBeFalsy();
+    });
   });
 });
