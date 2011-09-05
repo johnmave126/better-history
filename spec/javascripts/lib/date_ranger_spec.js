84dc63b3f867453120a08d70dd7050cf4b7cb01e
Roy Kolak <roy.kolak@gmail.com>
2011-09-05 17:31:20 -0500

    fixed date sensitive specs

--- spec/javascripts/lib/date_ranger_spec.js
+++ spec/javascripts/lib/date_ranger_spec.js
@@ -63,75 +63,84 @@ describe('DateRanger', function() {
     });
   });
 
-  describe('.today', function() {
-    it('returns date ranges for today', function() {
-      var range = DateRanger.today();
-      var date = new Date().getDate();
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
-    });
-  });
+  describe('creating past dates', function() {
+    var pastDate;
 
-  describe('.yesterday', function() {
-    it('returns date ranges for yesterday', function() {
-      var range = DateRanger.yesterday();
-      var date = new Date().getDate() - 1;
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
+    beforeEach(function() {
+      var date = new Date();
+      pastDate = function(day) {
+        return new Date(date.setDate(date.getDate() - day)).getDate();
+      };
     });
-  });
 
-  describe('.twoDaysAgo', function() {
-    it('returns date ranges for 2 days ago', function() {
-      var range = DateRanger.twoDaysAgo();
-      var date = new Date().getDate() - 2;
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
+    describe('.today', function() {
+      it('returns date ranges for today', function() {
+        var range = DateRanger.today(),
+            date = pastDate(0);
+
+        expect(range.start.getDate()).toEqual(date);
+        expect(range.end.getDate()).toEqual(date);
+      });
     });
-  });
 
-  describe('.threeDaysAgo', function() {
-    it('returns date ranges for 3 days ago', function() {
-      var range = DateRanger.threeDaysAgo();
-      var date = new Date().getDate() - 3;
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
+    describe('.yesterday', function() {
+      it('returns date ranges for yesterday', function() {
+        var range = DateRanger.yesterday(),
+            date = pastDate(1);
+
+        expect(range.start.getDate()).toEqual(date);
+        expect(range.end.getDate()).toEqual(date);
+      });
     });
-  });
 
-  describe('.fourDaysAgo', function() {
-    it('returns date ranges for 4 days ago', function() {
-      var range = DateRanger.fourDaysAgo();
-      var date = new Date().getDate() - 4;
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
+    describe('.twoDaysAgo', function() {
+      it('returns date ranges for 2 days ago', function() {
+        var range = DateRanger.twoDaysAgo(),
+            date = pastDate(2);
+
+        expect(range.start.getDate()).toEqual(date);
+        expect(range.end.getDate()).toEqual(date);
+      });
     });
-  });
 
-  describe('.fiveDaysAgo', function() {
-    it('returns date ranges for 5 days ago', function() {
-      var range = DateRanger.fiveDaysAgo();
-      var date = new Date().getDate() - 5;
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
+    describe('.threeDaysAgo', function() {
+      it('returns date ranges for 3 days ago', function() {
+        var range = DateRanger.threeDaysAgo(),
+            date = pastDate(3);
+
+        expect(range.start.getDate()).toEqual(date);
+        expect(range.end.getDate()).toEqual(date);
+      });
     });
-  });
 
-  describe('.sixDaysAgo', function() {
-    it('returns date ranges for 6 days ago', function() {
-      var range = DateRanger.sixDaysAgo();
-      var date = new Date().getDate() - 6;
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
+    describe('.fourDaysAgo', function() {
+      it('returns date ranges for 4 days ago', function() {
+        var range = DateRanger.fourDaysAgo(),
+            date = pastDate(4);
+
+        expect(range.start.getDate()).toEqual(date);
+        expect(range.end.getDate()).toEqual(date);
+      });
     });
-  });
 
-  describe('.sevenDaysAgo', function() {
-    it('returns date ranges for 7 days ago', function() {
-      var range = DateRanger.sevenDaysAgo();
-      var date = new Date().getDate() - 7;
-      expect(range.start.getDate()).toEqual(date);
-      expect(range.end.getDate()).toEqual(date);
+    describe('.fiveDaysAgo', function() {
+      it('returns date ranges for 5 days ago', function() {
+        var range = DateRanger.fiveDaysAgo(),
+            date = pastDate(5);
+
+        expect(range.start.getDate()).toEqual(date);
+        expect(range.end.getDate()).toEqual(date);
+      });
+    });
+
+    describe('.sixDaysAgo', function() {
+      it('returns date ranges for 6 days ago', function() {
+        var range = DateRanger.sixDaysAgo(),
+            date = pastDate(6);
+
+        expect(range.start.getDate()).toEqual(date);
+        expect(range.end.getDate()).toEqual(date);
+      });
     });
   });
 });
