0e089b5159723f6b1dc26901b956b3307eaa4f8e
Roy Kolak <roy.kolak@gmail.com>
2012-11-12 18:47:59 -0600

    Move mixin from lib to frameworks

---
+++ extension/javascripts/frameworks/mixin.js
@@ -0,0 +1,44 @@
+// From http://arcturo.github.com/library/coffeescript/03_classes.html
+
+var moduleKeywords,
+  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+moduleKeywords = ['extended', 'included'];
+
+BH.Lib.Module = (function() {
+
+  function Module() {}
+
+  Module.extend = function(obj) {
+    var key, value, _ref;
+    for (key in obj) {
+      value = obj[key];
+      if (__indexOf.call(moduleKeywords, key) < 0) {
+        this[key] = value;
+      }
+    }
+    if ((_ref = obj.extended) != null) {
+      _ref.apply(this);
+    }
+    return this;
+  };
+
+  Module.include = function(obj) {
+    var key, value, _ref;
+    for (key in obj) {
+      value = obj[key];
+      if (__indexOf.call(moduleKeywords, key) < 0) {
+        this.prototype[key] = value;
+      }
+    }
+    if ((_ref = obj.included) != null) {
+      _ref.apply(this);
+    }
+    return this;
+  };
+
+  return Module;
+
+})();
+
+Backbone.Model.include = Backbone.View.include = Backbone.Collection.include = BH.Base.include = BH.Lib.Module.include;
